44b79ef26b93ccef297d4c583ee21a558bd77748
mux <maxime.fellrath@gmail.com>
2022-12-17 10:57:42 +0100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# secret-santa
+# Secret-Santa in Python
+
+## Merry Christmas!
+
+This super simple python script will send to all your friends who they need to give a gift to. Note that although this script is wonderful and simple to use, the first step that is to collect your friends mail can be a pain in the ass.
+
+### Create participants list `participants.csv`.
+-  If you want someone to avoid giving a gift to someone that they find impish, add them to the `impish_list` field. You can add multiple naughty persons given you separate them using colons `:` (Note that the relation is asymmetric, Chris don't want to give to Will but Will could be ending up giving a present to Chris.)
+
+```
+name,mail,impish_list
+James,James.Cameron@gmail.com,
+Will,Will.Smith@gmail.com,
+Chris,Chris.Rock@gmail.com,Will
+```
+
+### Setup your secret file `secrets.json`. 
+To obtain your secret password, follow this [tutorial](https://stackoverflow.com/questions/72478573/how-to-send-an-email-using-python-after-googles-policy-update-on-not-allowing-j), provided by `X0-user-0X` <3
+
+```
+{
+    "EMAIL_SENDER": "MySuperGMAILAccount@gmail.com",
+    "PASSWORD": "passwooord"
+}
+```
--- merry_christmas.py
+++ merry_christmas.py
@@ -1,49 +1,93 @@
 import argparse
 import pandas as pd
 import random
-from rich.console import Console
+from rich.progress import track
+import json
+from email.mime.text import MIMEText
+import pickle
 import smtplib, ssl
-
-port = 587  # For starttls
-smtp_server = "smtp.gmail.com"
-sender_email = "mail@gmail.com"
-password = "password"
 
 class SecretSanta:
     def __init__(self,klaus_list, chain_number=1,blind=False):
+        """
+        The list is shuffled wrt the seed given, because I know youll forget which seed you put in it will dumnp the pairings in a file, in case anything goes wrong...
+        """
         self.klaus_list = klaus_list
         random.shuffle(klaus_list)
         self.chain_number = chain_number
         self.blind = blind
         self.number_of_participants = len(klaus_list)
+        with open('pairings.pkl', 'wb') as f:
+            pickle.dump(self.klaus_list, f)
 
-    def create_pairing(self)->dict:
+    def create_pairing(self):
+        """
+        Creates a pairing list by taking into account the impish list. This function has been implemented by a blind donkey, and has a worst case complexity probably of O(Tree(n!)), use it with care.
+        """
         self.giver_receiver = dict()
         for index, klaus in enumerate(self.klaus_list):
             self.giver_receiver[klaus.name] = klaus_list[(index+1)%self.number_of_participants]
+        for index, klaus in enumerate(self.klaus_list):
+            if not (klaus.impish_list==0):
+                if self.giver_receiver[klaus.name].name in (klaus.impish_list):
+                    random.shuffle(self.klaus_list)
+                    self.create_pairing()
         return self.giver_receiver
 
     def show_pairing(self):
+        """
+        Show the pairing list
+        """
         for key, value in self.giver_receiver.items() :
-            console.print(f"[bold] {key:<15} -> {value.name:<15}")
-    def send_game(self):
-        for klaus in self.klaus_list:
-            message = f"""\
-                Subject: Secret_Santa
+            print(f"{key:<18} -> {value.name:<18}")
+            
+    def send_game(self,secrets):
+        """
+        Send game to all participants by mail.
+        """
+        smtp_server = "smtp.gmail.com"
+        port = 587  
+        email_sender = secrets["EMAIL_SENDER"]
+        password =secrets["PASSWORD"]
+        context = ssl.create_default_context()
+        for klaus in track(self.klaus_list,description="Sending HOHOHOs"):
+            # Don't forget to change the mail content, unless your name is stan.
+            message=MIMEText(f"""\
+            Dear {klaus.name}! 
+            
+            You will be the secret santa of: {self.giver_receiver[klaus.name].name}.
 
-                Hello {klaus.name}, you will be the secret santa of: {self.giver_receiver[klaus.name]}.
-                """
-            with smtplib.SMTP(smtp_server, port) as server:
-                server.starttls()
-                server.login(sender_email, password)
-                server.sendmail(sender_email, klaus.mail, message)
-                server.quit()
+            Yours sincerely,
+
+            Stan
+            """)
+
+            email_receiver=klaus.mail
+            message["To"] = klaus.mail
+            message['From'] = email_sender
+            message['Subject'] = 'Secret Santa HO HO HO'
+
+            try:
+                server = smtplib.SMTP(smtp_server,port)
+                server.starttls(context=context) 
+                server.login(email_sender, password)
+                server.sendmail(email_sender, email_receiver, message.as_string())
+
+            except Exception as e:
+                print(e)
+            finally:
+                server.quit() 
+            
 # In a secret santa, everyone participant is a santa klaus.
 class Klaus:
-    def __init__(self,name,mail,belsnickel=None):
-        self.name = name
+    def __init__(self,name,mail,impish_list=None):
+        self.name = name.replace("_"," ")
         self.mail = mail
-        self.belsnickel = belsnickel
+        self.impish_list = impish_list
+
+        if not (self.impish_list==0):
+            print(self.impish_list)
+            self.impish_list=self.impish_list.replace("_"," ").split(":")
     
 def parse_args():
     """
@@ -51,23 +95,35 @@ def parse_args():
     :return results: Namespace of the arguments to pass to the main run function.
     """
     parser = argparse.ArgumentParser()
-
-
-
-
+    parser.add_argument('--send', action='store_true',default=False,help="Add this arg to send all mails.")
+    parser.add_argument('--blind', action='store_true',default=False,help="Add this arg to not be spoiled")
+    parser.add_argument('--seed', type=int,default=-1 ,help='Seed for the pairing randomness')
     results = parser.parse_args()
 
     return results
 
+
 if __name__ == '__main__':
+    try:
+        with open("secrets.json", mode='r') as f:
+            secrets = json.loads(f.read())
+    except FileNotFoundError:
+            print("filenotfound")
     args = parse_args()
+    if args.seed>=0:
+        random.seed(args.seed)
+    
     participants = pd.read_csv("participants.csv")
-    console = Console()
+    participants['impish_list']= participants['impish_list'].fillna(0)
     klaus_list = []
     for _, klaus in participants.iterrows():
-        klaus_list.append(Klaus(klaus['name'],klaus['mail'],klaus['belsnickel']))
+        klaus_list.append(Klaus(klaus['name'],klaus['mail'],klaus['impish_list']))
     secret_santa = SecretSanta(klaus_list)
     secret_santa.create_pairing()
-    secret_santa.show_pairing()
-    secret_santa.send_game()
-
+    if not args.blind:
+        secret_santa.show_pairing()
+        secret_santa.write_list()
+    if args.send:
+        secret_santa.send_game(secrets)
+    
+    
